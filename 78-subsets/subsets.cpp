58803aa7336d88ba0a73f44042da3c4f165f6624
Anushka Sharma <77441008+gitanushka@users.noreply.github.com>
2023-08-24 20:39:12 +0530

    Time: 7 ms (14.12%) | Memory: 16 MB (24.92%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void sub(int i, vector<int>& nums, vector<int> temp){
+        //base case
+        if(i==nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+
+        sub(i+1,nums,temp);
+        temp.push_back(nums[i]);
+        sub(i+1,nums,temp);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        sub(0,nums,temp);
+        return ans;
+    }
+};
